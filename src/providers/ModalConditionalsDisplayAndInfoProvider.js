0600d998efa133318248d865696fec4ec7e1ac23
Toriong <67920345+Toriong@users.noreply.github.com>
2021-05-08 22:33:08 -0500

    second commit

---
+++ src/providers/ModalConditionalsDisplayAndInfoProvider.js
@@ -0,0 +1,38 @@
+import React, { useState, createContext } from 'react'
+
+export const ModalConditionalsDisplayAndInfoContext = createContext()
+
+// the conditionals for opening a modal and what will be displayed on the modal
+export const ModalConditionalsDisplayAndInfoProvider = (props) => {
+
+    // selectedAddOns = the potential addOns that the user selected, but may be unselected when determining their order
+    const [selectedAddOns, setSelectedAddOns] = useState([{ name: null, price: 0 }]);
+    // the meat item info from the search bar will be stored in this state value directly below
+    const [orderInfoFromSearchBar, orderInfoFromSeachBar] = useState('');
+
+    // what buttons will be on the modal
+    const [isRemoveButtonOnModal, setIsRemoveButtonOnModal] = useState(false);
+    const [isUpdateButtonOnModal, setIsUpdateButtonOnModal] = useState(false);
+
+    // the conditionals for opening the modal
+    const [isModalWithCartInfoOpen, setIsModalWithCartInfoOpen] = useState(false);
+    // is the meat item modal displaying the meat item from the search bar? 
+    const [isMeatModalFromSearchBarOpen, setIsMeatModalFromSearchBarOpen] = useState(false);
+    const [isSearchResultsContainerOpen, setIsSearchResultsContainerOpen] = useState(false);
+    const [isNavModalOpen, setIsNavModalOpen] = useState(false);
+
+    return <ModalConditionalsDisplayAndInfoContext.Provider value={{
+        _openMeatItemModalFromSearchBar: [isMeatModalFromSearchBarOpen, setIsMeatModalFromSearchBarOpen],
+        _isButtonToRemoveOnDom: [isRemoveButtonOnModal, setIsRemoveButtonOnModal],
+        _isUpdateButtonOnDom: [isUpdateButtonOnModal, setIsUpdateButtonOnModal],
+        _selectedAddOns: [selectedAddOns, setSelectedAddOns],
+        _isModalWithCartInfoOpen: [isModalWithCartInfoOpen, setIsModalWithCartInfoOpen],
+        _isOrderMeatOrGoToRestrModalOpen: [isNavModalOpen, setIsNavModalOpen],
+        _isSearchResultsContainerOpen: [isSearchResultsContainerOpen, setIsSearchResultsContainerOpen],
+        _orderInfoFromSearchBar: [orderInfoFromSearchBar, orderInfoFromSeachBar]
+    }}>
+        {props.children}
+    </ModalConditionalsDisplayAndInfoContext.Provider>
+};
+
+
